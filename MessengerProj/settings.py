e2db738a41295ed6e381d2dbaf0a7efb3086fe58
Arvansh <kurreyminaxi@gmail.com>
2025-07-21 14:27:27 +0530

    Update settings.py

--- MessengerProj/settings.py
+++ MessengerProj/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('django-insecure-9*43m!hu(1ivstt!-lk%4^a6pq0l(8z0eu%39ueqf%(7m4a$tx')
+SECRET_KEY = os.environ.get('django-insecure-9*43m!hu(1ivstt!-lk%4^a6pq0l(8z0eu%39ueqf%(7m4a$tx', 'dev-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['massenger.onrender.com']
 
 
 # Application definition
