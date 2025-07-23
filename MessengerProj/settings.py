4339bb141a0ce8d2d933fc99d7c486717907a1ed
Arvansh <kurreyminaxi@gmail.com>
2025-07-23 17:50:26 +0530

    Update settings.py

--- MessengerProj/settings.py
+++ MessengerProj/settings.py
@@ -133,3 +133,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING  ={
+    'version':1,
+    'handlers':{
+        'class':'logging.StreamHandler',
+    },
+    'root':{
+        'handlers':['console'],
+        'level':'DEBUG',
+    },
+}
+    
