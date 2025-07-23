31654f5f00767d385028bb175861ca8d57b5c227
Arvansh <kurreyminaxi@gmail.com>
2025-07-23 16:25:39 +0530

    Update settings.py

--- MessengerProj/settings.py
+++ MessengerProj/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver._nostatic',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'massenger',
 ]
